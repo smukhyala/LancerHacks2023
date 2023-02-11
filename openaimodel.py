624663804ef205c6d63c94d4ab5d8b866963c6fe
smukhyala <smukhyala@gmail.com>
2023-02-11 12:22:54 -0800

    catching up to github

--- openaimodel.py
+++ openaimodel.py
@@ -1,9 +1,9 @@
-import openai
+import openaimodel
 # Set the API key
-openai.api_key = “YOUR_API_KEY”
+openaimodel.api_key = “YOUR_API_KEY”
 # Use the ChatGPT model to generate text
 model_engine = “text-davinci-002”
 prompt = “Hello, how are you today?”
-completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1,stop=None,temperature=0.7)
+completion = openaimodel.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1,stop=None,temperature=0.7)
 message = completion.choices[0].text
 print(message)
