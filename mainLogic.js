311587923683d4bb7a0e77626bce75ec153cb0c3
smukhyala <smukhyala@gmail.com>
2023-02-12 11:03:47 -0800

    Clean up

--- mainLogic.js
+++ mainLogic.js
@@ -1,8 +1,8 @@
+// start quiz logic
 const startQuizButton = document.querySelector('#start-quiz-button');
 startQuizButton.addEventListener('click', function() {
   const textContainer = document.querySelector('#text-container');
   textContainer.style.display = 'block';
-  
   let html = '';
   for (const question of questions) {
     html += `<p>${question.question}</p>`;
@@ -15,11 +15,13 @@ startQuizButton.addEventListener('click', function() {
   textContainer.innerHTML = html;
 });
 
+
+
+// annswer button logic
 const startAnswerButton = document.querySelector('#start-answer-button');
 startQuizButton.addEventListener('click', function() {
   const textContainer = document.querySelector('#text-container');
   textContainer.style.display = 'block';
-  
   let html = '';
   for (const question of questions) {
     html += `<p>${question.question}</p>`;
@@ -32,6 +34,9 @@ startQuizButton.addEventListener('click', function() {
   textContainer.innerHTML = html;
 });
 
+
+
+//toggling explanation on and off
 function toggleAnswer() {
   if (answer.style.visibility === "hidden") {
     answer.style.visibility = "visible";
@@ -40,6 +45,9 @@ function toggleAnswer() {
   }
 }
 
+
+
+// adding the quiz text to the screen
 function unhide() {
     var hids = document.getElementsByClassName("exp");
     for (var i = 0; i < hids.length; i++) {
@@ -48,17 +56,18 @@ function unhide() {
     answer.style.visibility = "hidden";
 }
 
+
+
+// answer bubble display
 const answerBubbles = document.querySelectorAll(".answer-bubble");
-                
 for (let i = 0; i < answerBubbles.length; i++) {
     answerBubbles[i].addEventListener("click", function() {
-    // Check if the answer is correct
+    // Check if the answer is correct (NEEDS TO BE ADAPTABLE TO EACH QUESTION)
     if (this.id === "answer3") {
         this.classList.add("correct");
     } else {
         this.classList.add("incorrect");
     }
-    
     // Disable the other answer bubbles
     for (let j = 0; j < answerBubbles.length; j++) {
         if (answerBubbles[j] !== this) {
@@ -68,25 +77,53 @@ for (let i = 0; i < answerBubbles.length; i++) {
     });
 }
 
+
+// Displaying current question
 let currentQuestionIndex = 0;
 const questionContainer = document.querySelector(".question");
 const answerContainers = document.querySelectorAll(".answer-bubble");
-
 function displayQuestion(question) {
     questionContainer.textContent = question.text;
     for (let i = 0; i < 4; i++) {
         answerContainers[i].textContent = question.answers[i].text;
     }
 }
-
 displayQuestion(questionsFinal[currentQuestionIndex]);
+document.getElementById("start-quiz-button").addEventListener("click", function() {
+  this.style.display = "none";
+  a.style.display = "none";
+  aa.style.display = "none";
+  });
 
+
+// changing the label contents
+function openLabel(evt, label) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(label).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// Display the "Home" div on page load
+document.addEventListener("DOMContentLoaded", function() {
+  openLabel(event, 'Home');
+});
+
+
+// asnycrounous on click function
 async function getQuestion() {
   const API_KEY = "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";
   const MODEL = "text-davinci-003";
   const PROMPT = "generate an " +  subject + "MCQ question and it's answer choices then explain why the correct answer is correct on a different line";
   const MAX_TOKENS = 500;
-
   const response = await fetch(`https://api.openai.com/v1/engines/text-davinci-003/completions`, {
     method: 'POST',
     headers: {
@@ -101,7 +138,6 @@ async function getQuestion() {
       temperature: 0.5
     })
   });
-
   const completion = await response.json();
   const text = completion.choices[0].text;
   const textArray = text.split("\n");
@@ -112,7 +148,6 @@ async function getQuestion() {
   const choiceD = textArray[7];
   let correctAnswer = null;
   let explanation = null;
-
   for (let i = 8; i < textArray.length; i++) {
     if (textArray[i].includes("Answer")) {
       correctAnswer = textArray[i];
@@ -124,7 +159,6 @@ async function getQuestion() {
       explanation = textArray[i];
     }
   }
-
   return {
     question,
     choiceA,
@@ -137,23 +171,22 @@ async function getQuestion() {
 }
 
 
-
+//loading bar (make it say loading until requested items exists)
 function showLoading() {
   document.querySelector('.loading').style.display = 'flex';
 }
-
 function hideLoading() {
   document.querySelector('.loading').style.display = 'none';
 }
 
 
+// starting async on next function
 async function next() {
     const questionData = await getQuestion();
     if (!questionData || !questionData.question || !questionData.choiceA || !questionData.choiceB || !questionData.choiceC || !questionData.choiceD || !questionData.explanation) {
         console.error('getQuestion() is not returning the expected data');
         return;
     }
-    
     var tq = questionData.question;
     var qone = questionData.choiceA;
     var qtwo = questionData.choiceB;
